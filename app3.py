d51545e0c7566947325a34df0456d4dae47d5fc4
Animesh Kumar <117807801+Animeshkr9044@users.noreply.github.com>
2023-12-28 16:44:38 +0530

    Add files via upload
    changed the enviormnet

--- app3.py
+++ app3.py
@@ -1,5 +1,7 @@
 import asyncio
+import queue
 import signal
+from typing import Optional
 
 import vocode
 from vocode.streaming.streaming_conversation import StreamingConversation
@@ -15,16 +17,19 @@ from vocode.streaming.synthesizer.play_ht_synthesizer import PlayHtSynthesizer
 from vocode.streaming.synthesizer.play_ht_synthesizer import PlayHtSynthesizerConfig
 from vocode.streaming.transcriber.deepgram_transcriber import DeepgramTranscriber
 
+
+# get_audio
+def get_audio(self) -> Optional[bytes]:
+    try:
+        return self.queue.get_nowait()
+    except queue.Empty:
+        return None
+
+
 # Set environment variables
-vocode.setenv(
-    OPEN_APT_KEY="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",
-    DEEPGRAM_API_KEY="ab1769cd27df5dc51f453c7ade74b77da7d452f9",
-    PLAY_HT_API_KEY="4eb0bf3bfc3a4f02bf6e52578b1c1bdb",
-    PLAY_HT_USER_ID="lpsjDH7W95VguTGS2LInizhdabQ2",
-)
 
 async def main():
-    microphone_input, speaker_output = create_streaming_microphone_input_and_speaker_output(
+    MicrophoneInput, speaker_output = create_streaming_microphone_input_and_speaker_output(
         use_blocking_speaker_output=False
     )
 
@@ -33,8 +38,9 @@ async def main():
         voice_id="s3://voice-cloning-zero-shot/1f44b3e7-22ea-4c2e-87d0-b4d9c8f1d47d/sophia/manifest.json",
         speed=None,  # You can set a value if needed
         preset=None,  # You can set a value if needed
-        sampling_rate=8000,  # Add the required field
+        sampling_rate=8000,  # mulaw set 8k sapling rate
         audio_encoding="mulaw",  # Add the required field
+
     )
 
     # Create StreamingConversation
@@ -42,7 +48,7 @@ async def main():
         output_device=speaker_output,
         transcriber=DeepgramTranscriber(
             DeepgramTranscriberConfig.from_input_device(
-                microphone_input,
+                MicrophoneInput,
                 endpointing_config=PunctuationEndpointingConfig(),
             )
         ),
@@ -58,8 +64,9 @@ async def main():
     await conversation.start()
     print("Conversation started, press Ctrl+C to end")
     while conversation.is_active():
-        chunk = await microphone_input.read(1024)  # Adjust buffer size as needed
+        chunk = MicrophoneInput.get_audio()  # Adjust buffer size as needed
         conversation.receive_audio(chunk)
+
 
 if __name__ == "__main__":
     asyncio.run(main())
